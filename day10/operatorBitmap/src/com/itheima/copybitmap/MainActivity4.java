689ff8fdbc81ca339b14b6ebe499b841846692dd
ymangu <ymangu@163.com>
2014-11-21 08:54:28 +0800

    project:图片的处理 功能：① 图片的拷贝；②图片的镜子效果 ③图片的旋转 ④图片的颜色变化

---
+++ day10/operatorBitmap/src/com/itheima/copybitmap/MainActivity4.java
@@ -0,0 +1,57 @@
+package com.itheima.copybitmap;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+	private ImageView iv1,iv2;
+	private Bitmap alterBitmap;
+	private Bitmap srcBmp;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		iv1 = (ImageView) findViewById(R.id.iv1);
+		iv2 = (ImageView) findViewById(R.id.iv2);
+		//给第一个imageview默认设置一个位图
+		srcBmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+		iv1.setImageBitmap(srcBmp);
+		//创建原图的一个副本。 可修改  创建的是一个空白的图形。
+		alterBitmap = Bitmap.createBitmap(srcBmp.getWidth()*2, srcBmp.getHeight()*2,srcBmp.getConfig());
+	}
+	/**
+	 * 创建原图 bm的一个拷贝。副本
+	 * @param view
+	 */
+	public void click(View view){
+		//1.准备一个画板  在上面放上准备好的 空白的位图
+		Canvas canvas = new Canvas(alterBitmap);
+		//2.准备一个画笔
+		Paint paint = new Paint();
+		paint.setColor(Color.BLACK);
+		//3.画画
+		Matrix m = new Matrix();
+		
+		ColorMatrix cm = new ColorMatrix();
+		cm.set(new float[] {
+		0.5f, 0, 0, 0, 0,
+		0, 0.8f, 0, 0, 0,
+		0, 0, 0.6f, 0, 0,
+		0, 0, 0, 1, 0
+		});
+		paint.setColorFilter(new ColorMatrixColorFilter(cm));
+		canvas.drawBitmap(srcBmp, m, paint);
+		iv2.setImageBitmap(alterBitmap);//把原图的副本设置到界面上。
+	}
+}
